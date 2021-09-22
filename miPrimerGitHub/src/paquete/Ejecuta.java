172c988558e7b8c6b683e78d11c83e08a764dd7e
javiv <javiv@LAPTOP-JCO00VT2>
2021-09-22 11:35:38 +0200

    JAVIER 22-09-2021 11:35 miPrimerGitHub - Ejecuta - finalizado Ej11

--- miPrimerGitHub/src/paquete/Ejecuta.java
+++ miPrimerGitHub/src/paquete/Ejecuta.java
@@ -1,5 +1,7 @@
 package paquete;
 
+import java.util.Scanner;
+
 public class Ejecuta {
 									/*ENUNCIADO	*/
 		/* Programa que lee nombre y año de nacimiento de una serie de alumnos hasta introducir
@@ -9,8 +11,29 @@ public class Ejecuta {
 
 	public static void main(String[] args) {
 		
+		String nombre,alumnoMenor="";
+		int año, suma=0, edad, cont=0, edadMenor=Integer.MAX_VALUE;
+		float media=0;
+		Scanner sc=new Scanner(System.in);
 		
-
+		do {
+		System.out.println("introduce nombre");
+		nombre=sc.nextLine();
+		
+		if(!nombre.equals("fin")) {
+			System.out.println("introduce año de nacimiento de " + nombre);
+			año=Integer.parseInt( sc.nextLine() );
+			edad=2021-año;
+			cont++;
+			suma += edad;
+			media= (float)suma/cont;
+			if(edadMenor>edad) {
+				edadMenor=edad;
+				alumnoMenor=nombre;
+			}
+		}
+		}while(!nombre.equals("fin"));
+		
+		System.out.println("la edad media es " + media + " y el alumno mas pequeño es " + alumnoMenor);
 	}
-
 }
