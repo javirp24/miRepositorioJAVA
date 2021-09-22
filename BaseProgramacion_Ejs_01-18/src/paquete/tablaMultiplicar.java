3e2f0c2a7e60a05a8f2cf7b8e53fd7894f0812fc
javiv <javiv@LAPTOP-JCO00VT2>
2021-09-22 18:36:52 +0200

    JAVIER 22-09-21 18:37 BaseProgramacion-tablaMultiplicar- cambios

--- BaseProgramacion_Ejs_01-18/src/paquete/tablaMultiplicar.java
+++ BaseProgramacion_Ejs_01-18/src/paquete/tablaMultiplicar.java
@@ -1,19 +1,43 @@
 package paquete;
 
+import java.util.Scanner;
+
 public class tablaMultiplicar {
 	
-									/*TABLA DE MULTIPLICAR*/
+								/*TODAS LAS TABLA DE MULTIPLICAR*/
 
 	public static void main(String[] args) {
-		int num=5;
+		/*
+		Scanner sc=new Scanner(System.in);
+		int numero;
 		int resultado;
 		String fila;
-		
+		System.out.println("que tabla de multiplicar quieres?");
+		numero= sc.nextInt();
 		for(int i=1; i<=10;i++) {
-			resultado=i*num;
-			fila=num + " X " + i + " = " + resultado;
+			resultado=i*numero;
+			fila=numero + " X " + i + " = " + resultado;
 			System.out.println(fila);
+		}*/
+		
+		
+		int resultado,suma;
+		for(int i = 1 ; i <= 10; i++) {
+			
+			System.out.println("TABLA DEL " + i + ": ");
+			suma=0;
+			for(int j = 0; j<=10; j++) {
+				
+				resultado= i * j;
+				suma +=resultado;
+				System.out.println(i + " X " + j + " = " + resultado);
+				
+				
+			}
+			System.out.println("la suma de la tabla del " + i + " es: " + suma);
+			System.out.println( "=============");
 		}
+		
 
 	}
 
